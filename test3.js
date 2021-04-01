9be32761829ec137be1bf13d9d4c5a15ca359647
Nutthaphon <nutthaphon.c@nellika.co.th>
2021-04-02 00:37:33 +0700

    reindent

--- test3.js
+++ test3.js
@@ -6,22 +6,22 @@ console.log(processType)
 function doRequest() {
 	return new Promise ((resolve, reject) => {
 		let options = {
-		  hostname: 'codequiz.azurewebsites.net',
-		  port: 80,
-		  method: 'GET',
-		  headers: {
-		    'Cookie': 'hasCookie=true'
-		  }
+		hostname: 'codequiz.azurewebsites.net',
+			port: 80,
+			method: 'GET',
+			headers: {
+				'Cookie': 'hasCookie=true'
+			}
 		};
 
 		http.get(options, function(res){
-		  res.setEncoding('utf8');
-		  res.on('data', (chunk) => {
-		    resolve(chunk)
-		  });
-		  res.on('end', () => {
-		    resolve('chunk')
-		  })
+		res.setEncoding('utf8');
+		res.on('data', (chunk) => {
+			resolve(chunk)
+		});
+		res.on('end', () => {
+			resolve('chunk')
+		})
 		});
 	})
 }
@@ -34,17 +34,17 @@ getRequestData = async function(){
 
 const app = http.createServer();
 app.on('request', async (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.writeHead(200, {'Content-Type': 'text/plain'});
 	let wholeBody = await doRequest();
 	let reg_val = '(?<='+processType+'\\<\\/td\\>\\<td\\>)(.+?)(?=\\<\\/td\\>)'
-    let re = new RegExp(reg_val);
-    let val = wholeBody.match(re)
-    let result_val = 'no_data';
-    if( val && val.length > 0){
-	    result_val = val[0];
-    }
+	let re = new RegExp(reg_val);
+	let val = wholeBody.match(re)
+	let result_val = 'no_data';
+	if( val && val.length > 0){
+		result_val = val[0];
+	}
 
-    res.end(result_val);
+	res.end(result_val);
 });
 
 app.listen(3000, 'localhost');
